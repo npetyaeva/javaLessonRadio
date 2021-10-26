7eee9589dd2a543f7c6eaca87366b3e3aa1413be
Natalia Petyaeva <npetyaeva@gmail.com>
2021-10-26 09:05:56 +0300

    Added new fields Min, Max

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -3,29 +3,31 @@ package ru.netology.domain;
 public class Radio {
 
     private int currentNumberStation;
+    private int NumberStationMin = 0;
+    private int NumberStationMax = 9;
 
     public int getCurrentNumberStation() {
         return currentNumberStation;
     }
 
     public void setNextNumberStation() {
-        if (currentNumberStation == 9) {
-            this.currentNumberStation = 0;
+        if (currentNumberStation == NumberStationMax) {
+            this.currentNumberStation = NumberStationMin;
             return;
         }
         this.currentNumberStation += 1;
     }
 
     public void setPrevNumberStation() {
-        if (currentNumberStation == 0) {
-            this.currentNumberStation = 9;
+        if (currentNumberStation == NumberStationMin) {
+            this.currentNumberStation = NumberStationMax;
             return;
         }
         this.currentNumberStation -= 1;
     }
 
     public int setNumberStation(int currentNumberStation) {
-        if (currentNumberStation < 0 || currentNumberStation > 9) {
+        if (currentNumberStation < NumberStationMin || currentNumberStation > NumberStationMax) {
             return this.currentNumberStation;
         }
         this.currentNumberStation = currentNumberStation;
